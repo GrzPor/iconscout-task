52d90a994e21cbc4688391affd71c9ab99b7f6a4
GrzPor <grzegorzporadko@gmail.com>
2025-04-17 22:48:24 +0200

    chore: [Eslint & Prettier] add tools

---
+++ eslint.config.js
@@ -0,0 +1,114 @@
+import js from '@eslint/js'
+import vuePlugin from 'eslint-plugin-vue'
+import nuxtPlugin from 'eslint-plugin-nuxt'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import vueParser from 'vue-eslint-parser'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/.nuxt/**',
+      '**/.output/**',
+      '**/.nitro/**',
+      '**/dist/**',
+      '**/logs/**',
+      '**/*.log',
+      '**/npm-debug.log*',
+      '**/yarn-debug.log*',
+      '**/yarn-error.log*',
+      '**/.env',
+      '**/.env.*',
+      '**/.cache/**',
+      '**/.idea/**',
+      '**/.vscode/**',
+      '**/.DS_Store',
+      '**/package-lock.json',
+      '**/yarn.lock',
+      '**/pnpm-lock.yaml',
+      '**/*.suo',
+      '**/*.ntvs*',
+      '**/*.njsproj',
+      '**/*.sln',
+      '**/*.sw?'
+    ]
+  },
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        withDefaults: 'readonly',
+        definePageMeta: 'readonly',
+        useNuxtApp: 'readonly',
+        useRuntimeConfig: 'readonly',
+        useRoute: 'readonly',
+        useRouter: 'readonly',
+        useFetch: 'readonly',
+        ref: 'readonly',
+        computed: 'readonly',
+        reactive: 'readonly',
+        onMounted: 'readonly',
+        watch: 'readonly'
+      }
+    },
+    plugins: {
+      vue: vuePlugin,
+      nuxt: nuxtPlugin,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      // Prettier
+      'prettier/prettier': 'error',
+
+      // Vue rules
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/attribute-hyphenation': 'error',
+      'vue/html-end-tags': 'error',
+      'vue/no-v-html': 'warn'
+    }
+  },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        defineNuxtConfig: 'readonly',
+        definePageMeta: 'readonly',
+        useNuxtApp: 'readonly',
+        useRuntimeConfig: 'readonly',
+        useRoute: 'readonly',
+        useRouter: 'readonly',
+        useFetch: 'readonly',
+        defineStore: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      // Prettier
+      'prettier/prettier': 'error',
+
+      // TypeScript rules (without requiring parserOptions.project)
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      // Common rules
+      'no-undef': 'off' // Turning this off as TypeScript handles most of these checks
+    }
+  }
+]
