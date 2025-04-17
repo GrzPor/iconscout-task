eeb18c8200f2bf8ea9324e05b076fcf29b104258
GrzPor <grzegorzporadko@gmail.com>
2025-04-17 20:59:11 +0200

    chore: [Nuxt] init framework

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <h1>Header</h1>
+        </header>
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
