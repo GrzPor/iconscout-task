d22d86106939f344844d12251664b298b01245b4
GrzPor <grzegorzporadko@gmail.com>
2025-04-18 23:28:18 +0200

    feat: [SubHeader] add component

---
+++ components/SubHeader/AppSubHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-subheader py-4">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: null,
+    required: true
+  },
+  description: {
+    type: String,
+    default: null,
+    required: false
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './AppSubHeader.scss';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <AppHeader />
+    <AppSubHeader
+      title="237 Limit 3D Illustrations"
+      description="248 3Ds exclusively selected by our designer community."
+    />
     <main>
       <slot />
     </main>
@@ -10,6 +14,7 @@
 
 <script setup lang="ts">
 import AppHeader from '@components/Header/AppHeader.vue'
+import AppSubHeader from '@components/SubHeader/AppSubHeader.vue'
 import AppFooter from '@components/Footer/AppFooter.vue'
 </script>
 
