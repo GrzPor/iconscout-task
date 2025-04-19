a893f1c718617c647c383c242b59daa1048aa4eb
GrzPor <grzegorzporadko@gmail.com>
2025-04-19 13:14:50 +0200

    feat: [SidebarToggle][CategoryTabs] add new components

---
+++ components/CategoryTabs/AppCategoryTabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul role="tablist" class="list-unstyled mb-0 d-flex align-items-end gap-4 category-tabs">
+    <li v-for="(tab, index) in tabs" :key="index" class="font-size-sm" role="presentation">
+      <NuxtLink :to="tab.path" role="tab">
+        {{ tab.name }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface Tab {
+  path: string
+  name: string
+}
+
+defineProps({
+  tabs: {
+    type: Array as () => Tab[],
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './AppCategoryTabs.scss';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,10 @@
       description="248 3Ds exclusively selected by our designer community."
     />
     <main>
+      <div class="d-flex">
+        <SidebarToggle />
+        <AppCategoryTabs :tabs="categories" />
+      </div>
       <slot />
     </main>
     <AppFooter />
@@ -16,6 +20,15 @@
 import AppHeader from '@components/Header/AppHeader.vue'
 import AppSubHeader from '@components/SubHeader/AppSubHeader.vue'
 import AppFooter from '@components/Footer/AppFooter.vue'
+import SidebarToggle from '@components/Sidebar/SidebarToggle/SidebarToggle.vue'
+import AppCategoryTabs from '@components/CategoryTabs/AppCategoryTabs.vue'
+const categories = [
+  { path: '/free-all-assets', name: 'All Assets' },
+  { path: '/free-3d-illustrations', name: '3D Illustrations' },
+  { path: '/free-lottie-animations', name: 'Lottie Animations' },
+  { path: '/free-illustrations', name: 'Illustrations' },
+  { path: '/free-icons', name: 'Icons' }
+]
 </script>
 
 <style scoped></style>
