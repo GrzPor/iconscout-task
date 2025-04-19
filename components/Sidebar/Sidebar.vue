720bd86aba036849bb8cc86f384a8408eeb6807d
GrzPor <grzegorzporadko@gmail.com>
2025-04-19 16:59:48 +0200

    feat: [Sidebar] add init components and structure

---
+++ components/Sidebar/Sidebar.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="app-sidebar">
+    <div class="app-sidebar__wrapper px-4">
+      <BFormCheckbox v-model="exclusiveIcons" switch reverse size="lg">
+        <span class="font-size-sm app-sidebar__label">Iconscout Exclusive</span>
+      </BFormCheckbox>
+    </div>
+    <SidebarRadioSection :items="radioSections" @toggle="toggleSection" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SidebarRadioSection from './SidebarRadioSection/SidebarRadioSection.vue'
+import { ref } from 'vue'
+
+const exclusiveIcons = ref(false)
+
+const radioSections = ref([
+  {
+    id: 'asset',
+    title: 'Asset',
+    isOpen: true,
+    options: [
+      {
+        name: 'asset',
+        value: 'all-assets',
+        label: 'All Assets'
+      },
+      {
+        name: 'asset',
+        value: '3D-illustrations',
+        label: '3D Illustrations'
+      },
+      {
+        name: 'asset',
+        value: 'lottie-animations',
+        label: 'Lottie Animations'
+      },
+      {
+        name: 'asset',
+        value: 'illustrations',
+        label: 'Illustrations'
+      },
+      {
+        name: 'asset',
+        value: 'icons',
+        label: 'Icons'
+      }
+    ]
+  },
+  {
+    id: 'price',
+    title: 'Price',
+    isOpen: true,
+    options: [
+      {
+        name: 'price',
+        value: 'free',
+        label: 'Free'
+      },
+      {
+        name: 'price',
+        value: 'premium',
+        label: 'Premium'
+      },
+      {
+        name: 'price',
+        value: 'all',
+        label: 'All'
+      }
+    ]
+  },
+  {
+    id: 'view',
+    title: 'View',
+    isOpen: true,
+    options: [
+      {
+        name: 'view',
+        value: 'pack',
+        label: 'Pack'
+      },
+      {
+        name: 'view',
+        value: 'individual',
+        label: 'Individual'
+      }
+    ]
+  },
+  {
+    id: 'sort-by',
+    title: 'Sort by',
+    isOpen: true,
+    options: [
+      {
+        name: 'sort-by',
+        value: 'popular',
+        label: 'Popular'
+      },
+      {
+        name: 'sort-by',
+        value: 'latest',
+        label: 'Latest'
+      },
+      {
+        name: 'sort-by',
+        value: 'featured',
+        label: 'Featured'
+      }
+    ]
+  }
+])
+
+function toggleSection(index) {
+  radioSections.value[index].isOpen = !radioSections.value[index].isOpen
+}
+</script>
+
+<style lang="scss" scoped>
+@use './Sidebar.scss';
+</style>
---
+++ components/Sidebar/SidebarRadioSection/SidebarRadioSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-for="(item, index) in items" :key="index" class="app-sidebar__wrapper px-4">
+    <button
+      @click="toggleSection(index)"
+      :aria-controls="`collapse-${item.id}`"
+      class="w-100 btn-zero app-sidebar__btn"
+    >
+      <span class="font-size-sm app-sidebar__label"> {{ item.title }} </span>
+      <img
+        src="@images/icons/angle-up.svg"
+        alt="angle-up"
+        :class="{ 'rotate-180': !item.isOpen }"
+      />
+    </button>
+
+    <BCollapse :id="`collapse-${item.id}`" v-model="item.isOpen">
+      <BaseRadioInput
+        v-for="(subitem, subindex) in item.options"
+        :key="subindex"
+        :id="subitem.value"
+        :name="subitem.name"
+        :value="subitem.value"
+        :label="subitem.label"
+      />
+    </BCollapse>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseRadioInput from '@/components/base/BaseRadioInput/BaseRadioInput.vue'
+
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+
+const emit = defineEmits(['toggle'])
+
+function toggleSection(index) {
+  emit('toggle', index)
+}
+</script>
+
+<style lang="scss" scoped>
+@use './SidebarRadioSection.scss';
+</style>
