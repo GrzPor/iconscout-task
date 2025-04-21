968b8c5bbc92d36fc4b496c54560c949756ce4c6
GrzPor <grzegorzporadko@gmail.com>
2025-04-21 18:21:22 +0200

    feat: [All Assets Page] add view more overlay with link for last item

--- components/base/BaseAssetTile/BaseAssetTile.vue
+++ components/base/BaseAssetTile/BaseAssetTile.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="asset-tile">
-    <img :src="url" :alt="name" class="asset-tile__image" />
-    <p class="font-size-xs mb-0 asset-tile__name">{{ name }}</p>
-    <div class="asset-tile__buttons" role="group" aria-label="Asset actions">
-      <button class="asset-tile__button" aria-label="Add to collection" title="Add to collection">
-        <FolderPlusIcon aria-hidden="true" />
-      </button>
-      <button class="asset-tile__button" aria-label="Download asset" title="Download asset">
-        <DownloadIcon aria-hidden="true" />
-      </button>
+    <div v-if="redirectUrl" class="asset-tile__overlay">
+      <p class="asset-tile__redirect-text">
+        View more
+        <ArrowTopRightIcon aria-hidden="true" />
+      </p>
     </div>
+    <a :href="redirectUrl || '#'" class="asset-tile__link">
+      <img :src="url" :alt="name" class="asset-tile__image" />
+      <p class="font-size-xs mb-0 asset-tile__name">{{ name }}</p>
+      <div class="asset-tile__buttons" role="group" aria-label="Asset actions">
+        <button class="asset-tile__button" aria-label="Add to collection" title="Add to collection">
+          <FolderPlusIcon aria-hidden="true" />
+        </button>
+        <button class="asset-tile__button" aria-label="Download asset" title="Download asset">
+          <DownloadIcon aria-hidden="true" />
+        </button>
+      </div>
+    </a>
   </div>
 </template>
 
 <script setup lang="ts">
+import ArrowTopRightIcon from '@images/icons/arrow-top-right.svg'
 import FolderPlusIcon from '@images/icons/folder-plus.svg'
 import DownloadIcon from '@images/icons/download.svg'
 
 defineProps<{
   url: string
   name: string
+  redirectUrl?: string
 }>()
 </script>
 
